e4d98e4abfa9cb5bc04262327f057b700c6505eb
Thomas Hayashi <thomashsh11@gmail.com>
2022-03-01 16:24:29 -0300

    Apples and Oranges in the house

---
+++ src/com/thomashayashi/AppleOrangeFall/AppleOrangeFall.java
@@ -0,0 +1,47 @@
+package com.thomashayashi.AppleOrangeFall;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AppleOrangeFall {
+    /*
+     * The function accepts following parameters:
+     *  1. INTEGER s - starting point of Sam's house location.
+     *  2. INTEGER t - ending location of Sam's house location.
+     *  3. INTEGER a - location of the Apple tree
+     *  4. INTEGER b - location of the Orange tree.
+     *  5. INTEGER_ARRAY apples - distances at which each apple falls from the tree
+     *  6. INTEGER_ARRAY oranges - distances at which each orange falls from the tree
+     */
+    public NumberFruit countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+        List<Integer> applesLocation = getFruitLocation(apples, a);
+        List<Integer> orangesLocation = getFruitLocation(oranges, b);
+
+        NumberFruit fruits = new NumberFruit();
+        fruits.apples = getNumberOfFruitInsideHouse(applesLocation, s, t);
+        fruits.oranges = getNumberOfFruitInsideHouse(orangesLocation, s, t);
+
+        return fruits;
+    }
+
+    private static List<Integer> getFruitLocation(List<Integer> fallenFruit, int treePosition) {
+        return fallenFruit.stream().map(fruit -> treePosition + fruit).collect(Collectors.toList());
+    }
+
+    private static int getNumberOfFruitInsideHouse(List<Integer> fruitLocation, int startHouse, int endHouse) {
+        int fruits = 0;
+        for(Integer appleLocation : fruitLocation)
+            if(isWithinTheHouse(appleLocation, startHouse, endHouse)) fruits++;
+        return fruits;
+    }
+
+    private static boolean isWithinTheHouse(Integer fruitLocation, int startHouse, int endHouse) {
+        return fruitLocation >= startHouse && fruitLocation <= endHouse;
+    }
+
+    public static class NumberFruit {
+        int apples = 0;
+        int oranges = 0;
+    }
+}
---
+++ src/com/thomashayashi/AppleOrangeFall/AppleOrangeFallTest.java
@@ -0,0 +1,128 @@
+package com.thomashayashi.AppleOrangeFall;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+* Easier to check the problem
+* https://www.hackerrank.com/challenges/apple-and-orange/problem
+*/
+public class AppleOrangeFallTest {
+    private AppleOrangeFall appleOrangeFall;
+
+    @BeforeEach
+    void setUp() {
+        appleOrangeFall = new AppleOrangeFall();
+    }
+
+    @Test
+    void noApplesNoOrangesHaveFallen() {
+        int startPointHouse = 3;
+        int endPointHouse = 5;
+        int appleTreeLocation = 0;
+        int orangeTreeLocation = 10;
+        List<Integer> fallenApplesLocation = new ArrayList<>();
+        List<Integer> fallenOrangesLocation = new ArrayList<>();
+
+        AppleOrangeFall.NumberFruit fruits = appleOrangeFall.countApplesAndOranges(
+                startPointHouse,
+                endPointHouse,
+                appleTreeLocation,
+                orangeTreeLocation,
+                fallenApplesLocation,
+                fallenOrangesLocation);
+
+        assertEquals(0, fruits.apples);
+        assertEquals(0, fruits.oranges);
+    }
+
+    @Test
+    void applesHaveFallenButNotInTheHouse() {
+        int startPointHouse = 3;
+        int endPointHouse = 5;
+        int appleTreeLocation = 0;
+        int orangeTreeLocation = 10;
+        List<Integer> fallenApplesLocation = Arrays.asList(-1,0,1);
+        List<Integer> fallenOrangesLocation = new ArrayList<>();
+
+        AppleOrangeFall.NumberFruit fruits = appleOrangeFall.countApplesAndOranges(
+                startPointHouse,
+                endPointHouse,
+                appleTreeLocation,
+                orangeTreeLocation,
+                fallenApplesLocation,
+                fallenOrangesLocation);
+
+        assertEquals(0, fruits.apples);
+        assertEquals(0, fruits.oranges);
+    }
+
+    @Test
+    void oneAppleIsInsideTheHouse() {
+        int startPointHouse = 3;
+        int endPointHouse = 5;
+        int appleTreeLocation = 0;
+        int orangeTreeLocation = 10;
+        List<Integer> fallenApplesLocation = Arrays.asList(-3,0,3);
+        List<Integer> fallenOrangesLocation = new ArrayList<>();
+
+        AppleOrangeFall.NumberFruit fruits = appleOrangeFall.countApplesAndOranges(
+                startPointHouse,
+                endPointHouse,
+                appleTreeLocation,
+                orangeTreeLocation,
+                fallenApplesLocation,
+                fallenOrangesLocation);
+
+        assertEquals(1, fruits.apples);
+        assertEquals(0, fruits.oranges);
+    }
+
+    @Test
+    void applesAreIsInsideTheHouse() {
+        int startPointHouse = 3;
+        int endPointHouse = 5;
+        int appleTreeLocation = 0;
+        int orangeTreeLocation = 10;
+        List<Integer> fallenApplesLocation = Arrays.asList(-3,0,3,4,5);
+        List<Integer> fallenOrangesLocation = new ArrayList<>();
+
+        AppleOrangeFall.NumberFruit fruits = appleOrangeFall.countApplesAndOranges(
+                startPointHouse,
+                endPointHouse,
+                appleTreeLocation,
+                orangeTreeLocation,
+                fallenApplesLocation,
+                fallenOrangesLocation);
+
+        assertEquals(3, fruits.apples);
+        assertEquals(0, fruits.oranges);
+    }
+
+    @Test
+    void applesAndOrangesAreIsInsideTheHouse() {
+        int startPointHouse = 3;
+        int endPointHouse = 5;
+        int appleTreeLocation = 0;
+        int orangeTreeLocation = 10;
+        List<Integer> fallenApplesLocation = Arrays.asList(-3,0,3,4,5);
+        List<Integer> fallenOrangesLocation = Arrays.asList(-6,-5,0,1,3);
+
+        AppleOrangeFall.NumberFruit fruits = appleOrangeFall.countApplesAndOranges(
+                startPointHouse,
+                endPointHouse,
+                appleTreeLocation,
+                orangeTreeLocation,
+                fallenApplesLocation,
+                fallenOrangesLocation);
+
+        assertEquals(3, fruits.apples);
+        assertEquals(2, fruits.oranges);
+    }
+}

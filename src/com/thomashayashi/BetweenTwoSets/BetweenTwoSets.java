13598898d6f20455d2a34e3feb756f1f1bc4d8e6
Thomas Hayashi <thomashsh11@gmail.com>
2022-03-02 15:12:56 -0300

    Between two sets

---
+++ src/com/thomashayashi/BetweenTwoSets/BetweenTwoSets.java
@@ -0,0 +1,32 @@
+package com.thomashayashi.BetweenTwoSets;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BetweenTwoSets {
+    public static int getTotalX(List<Integer> numberFactors, List<Integer> factoredByNumber){
+        Integer minElement = getMinValue(factoredByNumber);
+
+        int possibleNumbers = 0;
+        for(int num = 1; num <= minElement; num++)
+            possibleNumbers+= isFactoredAndAFactor(numberFactors, factoredByNumber, num) ? 1 : 0;
+
+        return possibleNumbers;
+    }
+
+    private static boolean isFactoredAndAFactor(List<Integer> numberFactors, List<Integer> factoredByNumber, int number) {
+        return (isFactoredByAll(numberFactors, number) && isFactorOfAll(factoredByNumber,number));
+    }
+
+    private static boolean isFactoredByAll(List<Integer> numberFactors, int number) {
+        return numberFactors.stream().allMatch(factor -> number % factor == 0);
+    }
+
+    private static boolean isFactorOfAll(List<Integer> factoredByNumber, int factor) {
+        return factoredByNumber.stream().allMatch(number -> number % factor == 0);
+    }
+
+    private static Integer getMinValue(List<Integer> list) {
+        return Collections.min(list);
+    }
+}
---
+++ src/com/thomashayashi/BetweenTwoSets/BetweenTwoSetsTest.java
@@ -0,0 +1,46 @@
+package com.thomashayashi.BetweenTwoSets;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+* Full description: https://www.hackerrank.com/challenges/between-two-sets/problem
+*/
+public class BetweenTwoSetsTest {
+    @Test
+    void whenThereIsOnlyFactorAndTheFactorIsTheSame_ThenOnlyOneNumberIsBetweenSets(){
+        List<Integer> factorsOfTheNumbers = Collections.singletonList(1);
+        List<Integer> numbersFactorAllElements = Collections.singletonList(1);
+        int numbersBetweenSets = BetweenTwoSets.getTotalX(factorsOfTheNumbers, numbersFactorAllElements);
+        assertEquals(1, numbersBetweenSets);
+    }
+
+    @Test
+    void whenTheFactorsArePrimes_ThenThereIsNoNumberBetweenSets(){
+        List<Integer> factorsOfTheNumbers = Collections.singletonList(2);
+        List<Integer> numbersFactorAllElements = Collections.singletonList(3);
+        int numbersBetweenSets = BetweenTwoSets.getTotalX(factorsOfTheNumbers, numbersFactorAllElements);
+        assertEquals(0, numbersBetweenSets);
+    }
+
+    @Test
+    void whenTheFactorsAreMultipleTakeTheBiggestMultipleAndFactorTheSmallestElement_ThenThreeNumbersAreExpected(){
+        List<Integer> factorsOfTheNumbers = Arrays.asList(2,4);
+        List<Integer> numbersFactorAllElements = Arrays.asList(16,32,96);
+        int numbersBetweenSets = BetweenTwoSets.getTotalX(factorsOfTheNumbers, numbersFactorAllElements);
+        assertEquals(3, numbersBetweenSets); // 4, 8 , 16
+    }
+
+    @Test
+    void whenTheFactorsAreNotMultipleTakeTheLowestCommonDenominatorAndFactorTheSmallestElement_ThenTwoNumbersAreExpected(){
+        List<Integer> factorsOfTheNumbers = Arrays.asList(3,4);
+        List<Integer> numbersFactorAllElements = Arrays.asList(24,48);
+        int numbersBetweenSets = BetweenTwoSets.getTotalX(factorsOfTheNumbers, numbersFactorAllElements);
+        assertEquals(2, numbersBetweenSets); // 12, 24
+    }
+}

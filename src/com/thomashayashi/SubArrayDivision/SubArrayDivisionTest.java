cce35c0f068d1f1920cdeeff667e04ebc8f2e9d5
Thomas Hayashi <thomashsh11@gmail.com>
2022-03-23 14:16:54 -0300

    Sub array division

--- src/com/thomashayashi/SubArrayDivision/SubArrayDivisionTest.java
+++ src/com/thomashayashi/SubArrayDivision/SubArrayDivisionTest.java
@@ -17,11 +17,12 @@ class SubArrayDivisionTest {
         assertEquals(0, birthday(Arrays.asList(1,1),1,2));
         assertEquals(1, birthday(Arrays.asList(1,1),2,2));
 
+        assertEquals(0, birthday(Arrays.asList(1,1,1,1,1),3,1));
         assertEquals(2, birthday(Arrays.asList(1,2,1,3,2),3,2));
         assertEquals(0, birthday(Arrays.asList(1,1,1,1,1),3,2));
     }
 
-    public int birthday(List<Integer> s, int d, int m) {
+    private int birthday(List<Integer> s, int d, int m) {
         int numOfDivisions = 0;
         int[] segments = s.stream().mapToInt(Integer::intValue).toArray();
         for(int i = 0; i < s.size(); i++) {

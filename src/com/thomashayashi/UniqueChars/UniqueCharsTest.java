83784af5ee4def107ba01c06837f2e9165795a62
Thomas Hayashi <thomashsh11@gmail.com>
2022-05-15 20:44:42 -0300

    Unique chars

--- src/com/thomashayashi/UniqueChars/UniqueCharsTest.java
+++ src/com/thomashayashi/UniqueChars/UniqueCharsTest.java
@@ -1,13 +1,10 @@
 package com.thomashayashi.UniqueChars;
 
-import kotlin.collections.CollectionsKt;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;

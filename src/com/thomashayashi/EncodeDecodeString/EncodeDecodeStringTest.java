9cd34d282674c1075dfe19d2138776ea8ca9e429
Thomas Hayashi <thomashsh11@gmail.com>
2022-07-08 20:30:41 -0300

    Encode string

--- src/com/thomashayashi/EncodeDecodeString/EncodeDecodeStringTest.java
+++ src/com/thomashayashi/EncodeDecodeString/EncodeDecodeStringTest.java
@@ -2,6 +2,7 @@ package com.thomashayashi.EncodeDecodeString;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,11 +18,38 @@ class EncodeDecodeStringTest {
         assertEquals("4#lint4#code4#love3#you", encode(Arrays.asList("lint","code","love","you")));
     }
 
-    private String encode(List<String> strs) {
+    private String encode(List<String> words) {
         StringBuilder encodedString = new StringBuilder();
-        for(String str: strs)
-            encodedString.append(str.length()).append("#").append(str);
+        for(String word: words)
+            encodedString.append(word.length()).append("#").append(word);
 
         return encodedString.toString();
+    }
+
+    @Test
+    void checkDecode() {
+        assertEquals(Arrays.asList("a","bc"), decode("1#a2#bc"));
+        assertEquals(Arrays.asList("lint","code","love","you"), decode("4#lint4#code4#love3#you"));
+    }
+
+    private List<String> decode(String encodedWords) {
+        List<String> decodedStrings = new ArrayList<>();
+        for(int i = 0; i < encodedWords.length(); i++) {
+            StringBuilder length = new StringBuilder();
+            while(encodedWords.charAt(i) != '#'){
+                length.append(encodedWords.charAt(i));
+                i++;
+            }
+            int wordLength = Integer.parseInt(length.toString());
+            i++;
+
+            StringBuilder word = new StringBuilder();
+            for(int j = i; j < wordLength+i; j++)
+                word.append(encodedWords.charAt(j));
+
+            decodedStrings.add(word.toString());
+            i = i + wordLength - 1;
+        }
+        return decodedStrings;
     }
 }

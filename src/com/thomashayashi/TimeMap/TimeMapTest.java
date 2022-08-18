4d32099560808b117a36cfdc12588f02368cdf0a
Thomas Hayashi <thomashsh11@gmail.com>
2022-08-18 10:46:29 -0300

    Time map

--- src/com/thomashayashi/TimeMap/TimeMapTest.java
+++ src/com/thomashayashi/TimeMap/TimeMapTest.java
@@ -2,52 +2,26 @@ package com.thomashayashi.TimeMap;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
  * Full description: https://leetcode.com/problems/time-based-key-value-store/
  */
 class TimeMapTest {
-    HashMap<String, List<Map.Entry<String, Integer>>> map;
+    TimeMap map;
 
     @Test
     void checkKnownTimeMap(){
-        map = new HashMap<>();
-        set("foo", "bar", 1);
-        assertEquals("bar", get("foo", 1));
+        map = new TimeMap();
+        map.set("foo", "bar", 1);
+        assertEquals("bar", map.get("foo", 1));
     }
 
     @Test
     void checkUnknownTimeMap(){
-        map = new HashMap<>();
-        set("foo", "bar", 1);
-        assertEquals("", get("fo", 1));
-    }
-
-    public void set(String key, String value, int timestamp) {
-        if (!map.containsKey(key)) map.put(key, new ArrayList<>());
-        map.get(key).add(new AbstractMap.SimpleEntry<>(value, timestamp) {
-        });
-    }
-
-    public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-        List<Map.Entry<String, Integer>> list = map.get(key);
-        return search(list, timestamp);
-    }
-
-    public String search(List<Map.Entry<String, Integer>> list, int timestamp) {
-        int start = 0;
-        int end = list.size() - 1;
-        while (start < end) {
-            int mid = start + (end - start + 1) / 2;
-            if (list.get(mid).getValue() <= timestamp) start = mid; else end =
-                    mid - 1;
-        }
-        return list.get(start).getValue() <= timestamp
-                ? list.get(start).getKey()
-                : "";
+        map = new TimeMap();
+        map.set("foo", "bar", 1);
+        assertEquals("", map.get("fo", 1));
     }
 }
+

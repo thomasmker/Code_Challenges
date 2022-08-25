9de780549854363647750b741d31c60895fa309e
Thomas Hayashi <thomashsh11@gmail.com>
2022-08-25 13:02:52 -0300

    Reverse Linked list

---
+++ src/com/thomashayashi/ReverseLinkedList/ReverseLinkedListTest.java
@@ -0,0 +1,69 @@
+package com.thomashayashi.ReverseLinkedList;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ * Full description: https://leetcode.com/problems/reverse-linked-list/
+ */
+class ReverseLinkedListTest {
+
+    @Test
+    void checkSingleNodeList(){
+        ListNode a = new ListNode("a");
+        checkReversedListResult(a, "a");
+    }
+
+    @Test
+    void checkTwoNodeList(){
+        ListNode a = new ListNode("a");
+        a.next = new ListNode("b");
+        checkReversedListResult(a, "b|a");
+    }
+
+    private void checkReversedListResult(ListNode originalList, String expectedResult) {
+        ListNode reversedList = ListNode.reverseList(originalList);
+        assertEquals(expectedResult, ListNode.printList(reversedList));
+    }
+}
+
+class ListNode {
+    public ListNode next;
+    private final String value;
+
+    public ListNode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode p;
+        ListNode q = null;
+        ListNode r = head;
+        while (r != null) {
+            p = q;
+            q = r;
+            r = r.next;
+            q.next = p;
+        }
+        return q;
+    }
+
+    public static String printList(ListNode head) {
+        List<String> list = new ArrayList<>();
+        while(head != null) {
+            list.add(head.getValue());
+            head = head.next;
+        }
+
+        return String.join("|", list);
+    }
+}

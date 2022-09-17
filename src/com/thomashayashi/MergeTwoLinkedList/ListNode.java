265396ff69c10bec4f3a322762e1be4ea188b333
ThomasHayashi <thomas.hayashi@pm.me>
2022-09-17 10:44:02 -0300

    Merge two linked list

--- src/com/thomashayashi/MergeTwoLinkedList/ListNode.java
+++ src/com/thomashayashi/MergeTwoLinkedList/ListNode.java
@@ -1,6 +1,30 @@
 package com.thomashayashi.MergeTwoLinkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNode {
     public int value;
     public ListNode next;
+
+    public ListNode() {
+
+    }
+    public ListNode(int value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return String.valueOf(this.value);
+    }
+
+    public String print(String delimiter) {
+        List<String> list = new ArrayList<>();
+        ListNode node = this;
+        do {
+            list.add(node.getValue());
+            node = node.next;
+        } while(node != null);
+
+        return String.join(delimiter, list);
+    }
 }

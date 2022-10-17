a990da5a50231d549e99bff6b14f4443fdd28abb
twnh <thomas.hayashi@pm.me>
2022-10-17 15:53:32 -0300

    Reorder linked list

--- src/com/thomashayashi/ReorderList/ReorderListTest.java
+++ src/com/thomashayashi/ReorderList/ReorderListTest.java
@@ -3,6 +3,9 @@ package com.thomashayashi.ReorderList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
@@ -40,12 +43,21 @@ public class ReorderListTest {
         Head.next = new ListNode(2);
         Head.next.next = new ListNode(3);
         Head.next.next.next = new ListNode(4);
-        assertResult("1|4|2|3");
+        assertResult(Arrays.asList("1","4","2","3"));
     }
 
     void assertResult(String expectedResult) {
         Head.reorderList();
         assertEquals(expectedResult, Head.print("|"));
+    }
+
+    void assertResult(List<String> expectedResult) {
+        Head.reorderList();
+        assertEquals(formatResult(expectedResult), Head.print("|"));
+    }
+
+    String formatResult(List<String> expectedResult) {
+        return String.join("|", expectedResult);
     }
 }
 

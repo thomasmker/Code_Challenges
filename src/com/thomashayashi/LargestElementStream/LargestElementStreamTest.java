2a27a8708b2bf7489eb49edad4f93491eeb0f23c
twnh <thomas.hayashi@pm.me>
2022-11-03 13:33:19 -0300

    Kth largest element

---
+++ src/com/thomashayashi/LargestElementStream/LargestElementStreamTest.java
@@ -0,0 +1,40 @@
+package com.thomashayashi.LargestElementStream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.PriorityQueue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ * Full description: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+ */
+public class LargestElementStreamTest {
+
+    @Test
+    void checkSingleNode() {
+        KthLargest heap = new KthLargest(1, List.of(0));
+        assertEquals(1, heap.add(1));
+    }
+}
+
+class KthLargest {
+
+    final PriorityQueue<Integer> heap = new PriorityQueue<>();
+    final int k;
+
+    public KthLargest(int k, List<Integer> numbers) {
+        this.k = k;
+        for (Integer n : numbers) add(n);
+    }
+
+    public Integer add(Integer val) {
+        if (heap.size() < k)heap.offer(val); //for adding the values of the array
+        else if (heap.peek() != null && val > heap.peek()) {
+            heap.poll(); //remove the top element
+            heap.add(val); //add the new element
+        }
+        return heap.peek();
+    }
+}
